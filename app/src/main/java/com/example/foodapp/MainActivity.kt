85c9c5b70e06063f67cb84bbdd755bffe439c009
LokiAngelMx <LokiGarciaMx@gmail.com>
2024-02-20 23:45:46 -0600

    Segunda pantalla agregada

--- app/src/main/java/com/example/foodapp/MainActivity.kt
+++ app/src/main/java/com/example/foodapp/MainActivity.kt
@@ -1,11 +1,21 @@
 package com.example.foodapp
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
+
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var buyBtn : Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        buyBtn = findViewById(R.id.buyBtn)
+        buyBtn.setOnClickListener {
+            val intent = Intent(this@MainActivity, BuyActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
